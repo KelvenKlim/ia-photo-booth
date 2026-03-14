dc673c0a080c68f3fac6c868273ba38a4ac2fee5
Kelven Prasad <kelvenn.jk14@gmail.com>
2026-03-14 16:18:16 -0400

    fix: error generate photo

--- ia_photo_booth_flutter/lib/services/api_service.dart
+++ ia_photo_booth_flutter/lib/services/api_service.dart
@@ -42,14 +42,12 @@ class ApiService {
     required String imageBase64,
   }) async {
     final uri = Uri.parse('$kApiBaseUrl/api/send-email');
-    final response = await http.post(
-      uri,
-      headers: {'Content-Type': 'application/json'},
-      body: jsonEncode({
-        'to_email': toEmail,
-        'image_base64': imageBase64,
-      }),
-    );
+    final request = http.MultipartRequest('POST', uri);
+    request.fields['to_email'] = toEmail;
+    request.fields['image_base64'] = imageBase64;
+
+    final streamed = await request.send();
+    final response = await http.Response.fromStream(streamed);
 
     if (response.statusCode != 200) {
       throw Exception('Erro ao enviar e-mail: ${response.reasonPhrase}');
