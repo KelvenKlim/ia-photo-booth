39c3f8e3baaead184bab295dec766f1280e9283a
Kelven Prasad <kelvenn.jk14@gmail.com>
2026-03-14 18:10:14 -0400

    feat: update API base URL to production endpoint

--- ia_photo_booth_flutter/lib/config.dart
+++ ia_photo_booth_flutter/lib/config.dart
@@ -1,2 +1,2 @@
 /// Change this to your backend URL (ngrok, local IP, etc.)
-const String kApiBaseUrl = 'http://192.168.100.54:8000';
+const String kApiBaseUrl = 'https://ia-photo-booth.onrender.com';
